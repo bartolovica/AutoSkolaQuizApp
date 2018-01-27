973c069c382822b7f85bf5b9f62f475bb9c90f42
bartolovica <antunbart@gmail.com>
2018-01-27 14:11:39 +0100

    3rd-part-works

---
+++ app/src/main/java/hr/fesb/autoskola250/androidonlinequizapp/Playing.java
@@ -0,0 +1,175 @@
+package hr.fesb.autoskola250.androidonlinequizapp;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import hr.fesb.autoskola250.androidonlinequizapp.Common.Common;
+
+public class Playing extends AppCompatActivity implements View.OnClickListener {
+
+    final static long INTERVAL = 10000;
+    final static long TIMEOUT = 70000;//1sekunda
+
+    CountDownTimer mCountDown;
+    int index = 0;
+    int score = 0;
+    int thisQuestion = 0;
+    int totalQuestion;
+    int correctAnswer;
+    int progressValue = 0;
+
+    //Firebase
+    FirebaseDatabase database;
+    DatabaseReference questions;
+
+    ProgressBar progressBar;
+    ImageView question_image;
+    Button btnA,btnB,btnC,btnD;
+    TextView txtScore;
+    TextView txtQuestionNum;
+    TextView question_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playing);
+
+        //firebase
+        database = FirebaseDatabase.getInstance();
+        questions = database.getReference("Questions");
+
+        //Views
+        txtScore = (TextView)findViewById(R.id.txtScore);
+        txtQuestionNum = (TextView)findViewById(R.id.txtTotalQuestion);
+        question_text =  (TextView)findViewById(R.id.question_text);
+        question_image = (ImageView)findViewById(R.id.question_image);
+
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+
+        btnA = (Button)findViewById(R.id.btnAnswerA);
+        btnB = (Button)findViewById(R.id.btnAnswerB);
+        btnC = (Button)findViewById(R.id.btnAnswerC);
+        btnD = (Button)findViewById(R.id.btnAnswerD );
+
+        btnA.setOnClickListener(this);
+        btnB.setOnClickListener(this);
+        btnC.setOnClickListener(this);
+        btnD.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        mCountDown.cancel();
+        if(index < totalQuestion) //sve dok imamo pitanja
+        {
+            Button clickedButton = (Button)view;
+            String rightAnswer = Common.questionList.get(index).getCorrectAnswer();
+            if(clickedButton.getText().equals(rightAnswer))
+            {
+                //ako izaberemo točan odgovor
+                score+=10;
+                correctAnswer++;
+                showQuestion(++index); //sljedeće pitanje
+            }
+            else
+            {
+                //ako izaberemo pogrešan odgovor
+                Intent intent = new Intent(this, Done.class);
+                Bundle dataSend = new Bundle();
+                dataSend.putInt("SCORE",score);
+                dataSend.putInt("TOTAL",totalQuestion);
+                dataSend.putInt("CORRECT",correctAnswer);
+                intent.putExtras(dataSend);
+                startActivity(intent);
+                finish();
+            }
+            txtScore.setText(String.format("%d",score));
+
+        }
+
+
+
+
+    }
+
+    private void showQuestion(int index) {
+        if (index < totalQuestion)
+        {
+            thisQuestion++;
+            txtQuestionNum.setText(String.format("%d / %d",thisQuestion,totalQuestion));
+            progressBar.setProgress(0);
+            progressValue=0;
+
+            if(Common.questionList.get(index).getIsImageQuestion().equals("true"))
+            {
+                //ako je pitanje-slika
+                Picasso.with(getBaseContext())
+                        .load(Common.questionList.get(index).getQuestion())
+                        .into(question_image);
+                question_image.setVisibility(View.VISIBLE);
+                question_text.setVisibility(View.INVISIBLE);
+            }
+            else
+            {   question_text.setText(Common.questionList.get(index).getQuestion());
+                question_image.setVisibility(View.INVISIBLE);
+                question_text.setVisibility(View.VISIBLE);
+            }
+
+            btnA.setText(Common.questionList.get(index).getAnswerA());
+            btnB.setText(Common.questionList.get(index).getAnswerB());
+            btnC.setText(Common.questionList.get(index).getAnswerC());
+            btnD.setText(Common.questionList.get(index).getAnswerD());
+
+            mCountDown.start();  //početak timera
+
+        }
+
+        else
+        {
+            //ako je zadnje pitanje
+            Intent intent = new Intent(this, Done.class);
+            Bundle dataSend = new Bundle();
+            dataSend.putInt("SCORE",score);
+            dataSend.putInt("TOTAL",totalQuestion);
+            dataSend.putInt("CORRECT",correctAnswer);
+            intent.putExtras(dataSend);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        totalQuestion = Common.questionList.size();
+        mCountDown = new CountDownTimer(TIMEOUT,INTERVAL) {
+            @Override
+            public void onTick(long minisec) {
+                progressBar.setProgress(progressValue);
+                progressValue++;
+            }
+
+            @Override
+            public void onFinish() {
+                mCountDown.cancel();
+                showQuestion(++index);
+
+            }
+        };
+        showQuestion(index);
+    }
+}
