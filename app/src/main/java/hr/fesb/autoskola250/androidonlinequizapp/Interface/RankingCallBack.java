84fc99b111eed8e4559bb4d8b9b52f826c3119d0
bartolovica <antunbart@gmail.com>
2018-02-11 20:52:41 +0100

    final-2

---
+++ app/src/main/java/hr/fesb/autoskola250/androidonlinequizapp/Interface/RankingCallBack.java
@@ -0,0 +1,10 @@
+package hr.fesb.autoskola250.androidonlinequizapp.Interface;
+
+/**
+ * Created by Antun on 27.1.2018..
+ */
+
+public interface RankingCallBack<T> {
+     void callBack(T ranking);
+
+}
