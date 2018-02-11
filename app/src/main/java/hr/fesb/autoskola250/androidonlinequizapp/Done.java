dddc7ea413eeb87e1a89d759fb4a1537fdb771c5
bartolovica <antunbart@gmail.com>
2018-02-11 17:49:18 +0100

    final version

---
+++ app/src/main/java/hr/fesb/autoskola250/androidonlinequizapp/Done.java
@@ -0,0 +1,76 @@
+package hr.fesb.autoskola250.androidonlinequizapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import hr.fesb.autoskola250.androidonlinequizapp.Common.Common;
+import hr.fesb.autoskola250.androidonlinequizapp.Model.QuestionScore;
+
+public class Done extends AppCompatActivity {
+    Button btnTryAgain;
+    TextView txtResultScore;
+    TextView getTxtResultQuestion;
+    ProgressBar progressBar;
+
+    FirebaseDatabase database;
+    DatabaseReference question_score;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_done);
+
+        database = FirebaseDatabase.getInstance();
+        question_score = database.getReference("Question_Score");
+
+        txtResultScore = (TextView)findViewById(R.id.txtTotalScore);
+        getTxtResultQuestion = (TextView)findViewById(R.id.txtTotalQuestion);
+        progressBar = (ProgressBar)findViewById(R.id.doneProgressBar);
+        btnTryAgain = (Button)findViewById(R.id.btnTryAgain);
+
+        btnTryAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Done.this,Home.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        //Get data from Bundle and set View
+        Bundle extra = getIntent().getExtras();
+        if(extra != null)
+        {
+            int score = extra.getInt("SCORE");
+            int totalQouestion = extra.getInt("TOTAL");
+            int correctAnswer = extra.getInt("CORRECT");
+
+            txtResultScore.setText(String.format("SCORE: %d",score));
+            getTxtResultQuestion.setText(String.format("PASSED: %d / %d",correctAnswer,totalQouestion));
+
+            progressBar.setMax(totalQouestion);
+            progressBar.setProgress(correctAnswer);
+
+            //Upload point to DB
+            question_score.child(String.format("%s_%s", Common.currentUser.getUserName(),
+                                                        Common.CategoryId))
+                    .setValue(new QuestionScore(String.format("%s_%s", Common.currentUser.getUserName(),
+                            Common.CategoryId),
+                            Common.currentUser.getUserName(),
+                            String.valueOf(score),
+                            Common.CategoryId,
+                            Common.categoryName));
+
+        }
+
+    }
+}
